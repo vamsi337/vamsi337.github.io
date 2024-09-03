7d340739d9b7d1f7324c9e4347eb2de39afcfea9
Vamsi Krishna <vamsijvk0337@gmail.com>
2024-09-03 13:13:28 +0530

    modified intro

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Backend development", "Frontend development", "web development"],
+    strings: ["Devops", "Cloud", "Backend Development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
